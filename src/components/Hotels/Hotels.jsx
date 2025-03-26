819d5a1b9a43ab6ed6c2360009428cb773abc06c
Adel <adeleslami0077@yahoo.com>
2025-03-27 03:21:09 +0330

    Search result item

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 
 function Hotels() {
@@ -11,7 +11,26 @@ function Hotels() {
     `q=${destination || ""}&accommodates_gte=${room || 1}`
   );
   if (isLoading) return <p>loading...</p>
-  return <div>{data.length}</div>;
+  return <div className="searchList">
+    <h2>Search Result({data.length})</h2>
+    {
+        data.map(item=>{
+            return <Link key={item.id} to={`/hotels/${item.id}?lat=${item.latitude}&lng=${item.longitude}`}>
+                <div className="searchItem">
+                    <img src={item.thumbnail_url} alt={item.name} />
+                    <div className="searchItemDesc">
+                        <p className="location">{item.smart_location}</p>
+                        <p className="name">{item.name}</p>
+                        <p className="price">
+                            €&nbsp;{item.price}&nbsp;
+                            <span>night</span>
+                        </p>
+                    </div>
+                </div>
+            </Link>
+        })
+    }
+  </div>;
 }
 
 export default Hotels;
