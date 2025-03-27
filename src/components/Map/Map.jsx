34cc306d987da5e549619106b8ae7bc2da5ffea3
Adel <adeleslami0077@yahoo.com>
2025-03-27 16:48:33 +0330

    Sync map center whit lat and lng

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useHotel } from "../../context/HotelContext";
 import Loader from "../Loader/Loader";
+import { useSearchParams } from "react-router-dom";
 
 function Map() {
   const [mapCenter, setMapCenter] = useState([51.505, -0.09]);
   const { isLoading, hotels } = useHotel();
+  const[searchParams ,setSearchParams]=useSearchParams()
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");  
+
+  useEffect(()=>{
+    if(lat && lng){
+      setMapCenter([lat,lng])
+    }
+  },[lat,lng])
   if (isLoading) return <Loader />;
   return (
     <div className="mapContainer">
@@ -15,6 +25,7 @@ function Map() {
         zoom={13}
         scrollWheelZoom={true}
       >
+        <ChangeCenter position={mapCenter} />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
@@ -33,3 +44,9 @@ function Map() {
 }
 
 export default Map;
+
+function ChangeCenter({position}){
+   const map = useMap()
+    map.setView(position)
+    return null
+}
