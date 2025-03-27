ae7aba1550b97edc73375d418fe16deb17609535
Adel <adeleslami0077@yahoo.com>
2025-03-27 16:18:08 +0330

    install react leaflet and complete Mpa component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useHotel } from "../../context/HotelContext";
+import Loader from "../Loader/Loader";
 
 function Map() {
+  const [mapCenter, setMapCenter] = useState([51.505, -0.09]);
+  const { isLoading, hotels } = useHotel();
+  if (isLoading) return <Loader />;
   return (
-    <div className="mapContainer">map</div>
-  )
+    <div className="mapContainer">
+      <MapContainer
+        className="map"
+        center={mapCenter}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {hotels.map((item) => {
+          return (
+            <Marker key={item.id} position={[item.latitude, item.longitude]}>
+              <Popup>{item.host_location}</Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
+      ,
+    </div>
+  );
 }
 
-export default Map
+export default Map;
